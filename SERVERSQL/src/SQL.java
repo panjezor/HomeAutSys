0b5c66070f6e34a969c8689506cd6674af46b25d
panjezor <filipjancieslik@gmail.com>
2019-01-22 21:58:33 +0000

    Corrections on Client to return data as a string SQL change to use java.sql.statement instead of jdbc (jdbc is something like an improvement to java.sql)

--- SERVERSQL/src/SQL.java
+++ SERVERSQL/src/SQL.java
@@ -4,7 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import com.mysql.jdbc.Statement;
+import java.sql.Statement;
 
 
 public class SQL {
